134b6ceb60a429625f1bd8aff26790b7fd873d9d
Won Kim <wonsae.kim@gmail.com>
2019-04-24 15:28:53 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -30,20 +30,6 @@ class Person
       @hygiene = level
     end
   end
-  
-  # def limit
-  #   if self.happiness > 10
-  #     self.happiness = 10
-  #   elsif self.happiness < 0
-  #     self.happiness = 0
-  #   end
-  
-  #   if self.hygiene > 10
-  #     self.hygiene = 10
-  #   elsif self.hygiene < 0
-  #     self.hygiene = 0
-  #   end
-  # end
 
   def clean?
     self.hygiene > 7
@@ -60,22 +46,18 @@ class Person
 
   def take_bath
     self.hygiene += 4
-    # self.limit
     "♪ Rub-a-dub just relaxing in the tub ♫"
   end
 
   def work_out
     self.happiness += 2
     self.hygiene -= 3
-    # self.limit
     "♪ another one bites the dust ♫"
   end
 
   def call_friend(person)
     self.happiness += 3
     person.happiness +=3
-    # self.limit
-    # person.limit
     "Hi #{person.name}! It's #{self.name}. How are you?"
   end
 
@@ -83,14 +65,10 @@ class Person
     if topic == "politics"
       self.happiness -= 2
       person.happiness -= 2
-    # self.limit
-    #   person.limit
       return "blah blah partisan blah lobbyist"
     elsif topic == "weather"
       self.happiness += 1
       person.happiness += 1
-      # self.limit
-      # person.limit
       return "blah blah sun blah rain"
     else
       return "blah blah blah blah blah"
